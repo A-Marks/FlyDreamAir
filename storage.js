674739b6de27f159979573d40f0aceb868a681c7
A-Marks <103034301+A-Marks@users.noreply.github.com>
2022-05-25 01:32:18 +1000

    Added storage.js
    Added file containing functions for test data initialisation.

---
+++ storage.js
@@ -0,0 +1,29 @@
+function initialise() {
+    const flights = [];
+    flights.push(["FlyDreamAir #3170", "26-06-2022/10:00", "Melbourne"]);
+    flights.push(["FlyDreamAir #3171", "26-06-2022/12:00", "Sydney"]);
+    flights.push(["FlyDreamAir #3172", "27-06-2022/15:00", "Melbourne"]);
+    flights.push(["FlyDreamAir #3173", "27-06-2022/15:15", "Brisbane"]);
+    flights.push(["FlyDreamAir #3174", "27-06-2022/17:00", "Adelaide"]);
+    flights.push(["FlyDreamAir #3175", "28-06-2022/8:30", "Melbourne"]);
+    localStorage.setItem("fdaflights", JSON.stringify(flights));
+
+    const menu = [];
+    menu.push(["Tomato Soup", "$6.00"]);
+    menu.push(["Bag of Peanuts", "$2.00"]);
+    menu.push(["Midori Sour", "$6.49"]);
+    menu.push(["Crab Bisque", "$10.50"]);
+    menu.push(["Aubergine Parmigiana", "$7.00"]);
+    menu.push(["Water", "$1.30"]);
+    menu.push(["Breadsticks", "$2.00"]);
+    localStorage.setItem("fdamenu", JSON.stringify(menu));
+
+    const bookings = [];
+    localStorage.setItem("fdabookings", JSON.stringify(bookings));
+}
+
+function purge() {
+    localStorage.removeItem("fdaflights");
+    localStorage.removeItem("fdamenu");
+    localStorage.removeItem("fdabookings");
+}
